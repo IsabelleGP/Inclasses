5ce883ff20d2421600fa7ac9866efbcf32a62452
IsabelleGP <69484978+IsabelleGP@users.noreply.github.com>
2021-06-15 11:40:51 -0300

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,324 @@
+<?php
+	include ("topo.php");
+?>
+
+	
+    <?php
+		include ("menu.php");
+	?>
+	
+	
+	
+	<!-- DIV PRINCIPAL DA PÁGINA PRINCIPAL-->
+    <main>
+        <div class="slider-area">
+            <div class="slider-active dot-style">
+                <!-- Slider Single -->
+                <div class="single-slider d-flex align-items-center slider-height">
+                    <div class="container">
+                        <div class="row align-items-center">
+                            <div class="col-xl-7 col-lg-8 col-md-10 ">
+                             <div class="hero-wrapper">
+							 
+                                <!-- ÍCONE DE VÍDEO -->
+                                <div class="video-icon">
+                                    <a class="popup-video btn-icon" href="https://www.youtube.com/watch?v=up68UAfH0d0" data-animation="bounceIn" data-delay=".4s">
+                                        <i class="fas fa-play"></i>
+                                    </a>
+                                </div>
+								
+								<!-- TEXTINHO NA DIV PRINCIPAL-->
+                                <div class="hero__caption">
+                                    <h1 data-animation="fadeInUp" data-delay=".3s">Inclusão e Acessibilidade</h1>
+                                    <p data-animation="fadeInUp" data-delay=".6s">***Software de compartilhamento de depoimentos para Pessoas com Deficiência (PcD)***</p>
+						
+                                </div>
+								
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>    
+			
+            <!-- Slider Single - OBS: não sei oq faz, não altera nada 
+            <div class="single-slider d-flex align-items-center slider-height">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-7 col-lg-8 col-md-10 ">
+                         <div class="hero-wrapper">
+                            <!-- Video icon 
+                            <div class="video-icon">
+                                <a class="popup-video btn-icon" href="https://www.youtube.com/watch?v=up68UAfH0d0" data-animation="bounceIn" data-delay=".4s">
+                                    <i class="fas fa-play"></i>
+                                </a>
+                            </div>
+                            <div class="hero__caption">
+                                <h1 data-animation="fadeInUp" data-delay=".3s">Health is wealth  keep it healthy </h1>
+                                <p data-animation="fadeInUp" data-delay=".6s">Almost before we knew it, we<br> had left the ground</p>
+                                <a href="#" class="btn" data-animation="fadeInLeft" data-delay=".3s">Take a Service</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>-->
+			
+        </div>    
+    </div>
+</div>
+<!-- FIM DA DIV PRINCIPAL -->
+
+
+
+<!--PARTE 2-->
+<div class="about-area2 section-padding40">
+    <div class="container">
+        <div class="row align-items-center">
+		
+		<!-- IMAGENS -->
+            <div class="col-lg-7 col-md-12">
+                <div class="about-img ">
+                    <img src="../assets/img/gallery/about.png" alt="">
+                </div>
+            </div>
+			
+            <div class="col-lg-5 col-md-12">
+                <div class="about-caption">
+                    <!-- SEGUNDO TÍTULO -->
+                    <div class="section-tittle mb-35">
+                        <h2>Título</h2>
+                    </div>
+					
+                    <p class="pera-top mb-40">Texto</p>
+                    <p class="pera-bottom mb-30">Texto</p>
+					
+					<!-- ÍCONES -->
+                    <div class="icon-about">
+						<img src="../assets/img/icon/about1.svg" alt="" class=" mr-20">
+						<img src="../assets/img/icon/about2.svg" alt="">
+					</div>
+					
+             </div>
+         </div>
+     </div>
+ </div>
+</div>
+<!-- FIM DA PARTE 2 -->
+
+
+
+<!-- PARTE 3 -->
+<section class="wantToWork-area section-bg3" data-background="../assets/img/gallery/section_bg01.png">
+    <div class="container">
+        <div class="wants-wrapper w-padding2">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-xl-7 col-lg-9 col-md-8">
+                    <div class="wantToWork-caption wantToWork-caption2">
+                        <h2>Título</h2>
+                        <p>Texto</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- QUADRADINHOS  -->
+<div class="service-area">
+    <div class="container">
+        <div class="row">
+			
+			<!-- Q1 -->
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-cat text-center mb-50">
+                    <div class="cat-icon">
+                        <img src="../assets/img/icon/services1.svg" alt="">
+                    </div>
+                    <div class="cat-cap">
+                        <h5><a href="services.html">Título</a></h5>
+                        <p>Texto</p>
+                        <a href="services.html" class="plus-btn"><i class="ti-plus"></i></a>
+                    </div>
+                </div>
+            </div>
+			
+			<!-- Q2 -->
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-cat text-center mb-50">
+                    <div class="cat-icon">
+                        <img src="../assets/img/icon/services2.svg" alt="">
+                    </div>
+                    <div class="cat-cap">
+                        <h5><a href="services.html">Título</a></h5>
+                        <p>Texto</p>
+                        <a href="services.html" class="plus-btn"><i class="ti-plus"></i></a>
+                    </div>
+                </div>
+            </div>
+			
+			<!-- Q3 -->
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-cat text-center mb-50">
+                    <div class="cat-icon">
+                        <img src="../assets/img/icon/services3.svg" alt="">
+                    </div>
+                    <div class="cat-cap">
+                        <h5><a href="services.html">Título</a></h5>
+                        <p>Texto</p>
+                        <a href="services.html" class="plus-btn"><i class="ti-plus"></i></a>
+                    </div>
+                </div>
+            </div>
+			
+        </div>
+    </div>
+</div>
+<!-- FIM DOS QUADRADINHOS -->
+<!-- FIM DA PARTE 3-->
+
+
+<!-- PARTE 4 (COMENTARIOS) -->
+<section class="testimonial-area testimonial-padding fix">
+    <div class="container">
+        <div class="row align-items-center justify-content-center">
+            <div class=" col-lg-9">
+                <div class="about-caption">
+				
+                    <!-- Testimonial Start -->
+                    <div class="h1-testimonial-active dot-style">
+					
+                        <!-- COMENTARIO 1 -->
+                        <div class="single-testimonial position-relative">
+                            <div class="testimonial-caption">
+                                <img src="assets/img/icon/quotes-sign.png" alt="" class="quotes-sign">
+                                <p>Texto</p>
+                            </div>
+                            <!-- founder -->
+                            <div class="testimonial-founder d-flex align-items-center">
+                                <div class="founder-img">
+                                    <img src="../assets/img/icon/testimonial.png" alt="">
+                                </div>
+                                <div class="founder-text">
+                                    <span>Nome</span>
+                                    <p>Profissão</p>
+                                </div>
+                            </div>
+                        </div>
+						
+						
+                        <!-- COMENTARIO 2 -->
+                        <div class="single-testimonial position-relative">
+                            <div class="testimonial-caption">
+                                <img src="../assets/img/icon/quotes-sign.png" alt="" class="quotes-sign">
+                                <p>Texto</p>
+                            </div>
+                            <!-- founder -->
+                            <div class="testimonial-founder d-flex align-items-center">
+                                <div class="founder-img">
+                                    <img src="../assets/img/icon/testimonial.png" alt="">
+                                </div>
+                                <div class="founder-text">
+                                    <span>Nome</span>
+                                    <p>Profissão</p>
+                                </div>
+                            </div>
+                        </div>
+						
+						
+                    </div>
+                    <!-- Testimonial End -->
+					
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- FIM DA PARTE 4 -->
+
+
+<!-- PARTE 5 -->
+<!-- Vídeo -->
+<div class="container">
+    <div class="video-area section-bg2 d-flex align-items-center"  data-background="../assets/img/gallery/video-bg.png">
+        <div class="video-wrap position-relative">
+            <div class="video-icon" >
+                <a class="popup-video btn-icon" href="https://www.youtube.com/watch?v=up68UAfH0d0"><i class="fas fa-play"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- FIM DA PARTE 5 -->   
+
+   
+<!-- PARTE 6 -->
+<section class="home-blog-area section-padding30">
+    <div class="container">
+	
+        <!-- Título -->
+        <div class="row justify-content-center">
+            <div class="col-lg-7 col-md-9 col-sm-10">
+                <div class="section-tittle text-center mb-100">
+                    <h2>Título</h2>
+                </div>
+            </div>
+        </div>
+		
+		<!-- BLOCO 1 -->
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="home-blog-single mb-40">
+                    <div class="blog-img-cap">
+                        <div class="blog-img">
+                            <img src="../assets/img/gallery/blog1.png" alt="">
+                        </div>
+                        <div class="blog-cap">
+                            <h3><a href="blog_details.html">Título</a></h3>
+                            <P>Texto</P>
+                        </div>
+                    </div>
+                </div>
+            </div>
+			
+			<!-- BLOCO 2 -->
+            <div class="col-lg-4 col-md-6">
+                <div class="home-blog-single mb-40">
+                    <div class="blog-img-cap">
+                        <div class="blog-img">
+                            <img src="../assets/img/gallery/blog2.png" alt="">
+                        </div>
+                        <div class="blog-cap">
+                            <h3><a href="blog_details.html">Título</a></h3>
+                            <P>Texto</P>
+                        </div>
+                    </div>
+                </div>
+            </div>
+			
+			<!-- BLOCO 3 -->
+            <div class="col-lg-4 col-md-6">
+                <div class="home-blog-single mb-40">
+                    <div class="blog-img-cap">
+                        <div class="blog-img">
+                            <img src="../assets/img/gallery/blog3.png" alt="">
+                        </div>
+                        <div class="blog-cap">
+                            <h3><a href="blog_details.html">Título</a></h3>
+                            <P>Texto</P>
+                        </div>
+                    </div>
+                </div>
+            </div>
+			
+        </div>
+    </div>
+</section>
+<!-- FIM DA PARTE 6 -->
+
+
+<?php
+	include ("rodape.php");
+ ?>
+
+
+<?php
+	include ("base.php");
+?>
