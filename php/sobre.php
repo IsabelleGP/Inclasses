df44a842f14c809b722232630c5fa596b4abf65c
IsabelleGP <69484978+IsabelleGP@users.noreply.github.com>
2021-06-15 11:41:24 -0300

    Create sobre.php

---
+++ php/sobre.php
@@ -0,0 +1,264 @@
+<?php
+	include ("topo.php");
+?>
+
+  
+	<?php
+		include ("menu.php");
+	?>
+	
+    <main>
+        <!--? Slider Area Start-->
+        <div class="slider-area slider-area2">
+            <div class="slider-active dot-style">
+                <!-- Slider Single -->
+                <div class="single-slider  d-flex align-items-center slider-height2">
+                    <div class="container">
+                        <div class="row align-items-center">
+                            <div class="col-xl-7 col-lg-8 col-md-10 ">
+                             <div class="hero-wrapper">
+                                <div class="hero__caption">
+                                    <h1 data-animation="fadeInUp" data-delay=".3s">Sobre nós</h1>
+                                    <p data-animation="fadeInUp" data-delay=".6s">Conheça mais sobre as criadoras e o projeto</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>    
+        </div>
+    </div>
+    <!-- Slider Area End -->
+    <!--? Team Area Start-->
+    <section class="team-area pb-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-8">
+                    <div class="single-cat text-center mb-30">
+                        <div class="cat-icon">
+                            <img src="../assets/img/gallery/team1.png" alt="">
+                        </div>
+                        <div class="cat-cap">
+                            <h5><a href="#">Susan Cristini Francalassi</a></h5>
+                            <p>Estudante do curso técnico em informática integrado ao ensino médo do Instituto Federal de São Paulo - Campus Campinas</p>
+                        </div>
+                    </div>
+                </div>
+				
+				
+                <div class="col-lg-4 col-md-6 col-sm-8">
+                    <div class="single-cat text-center mb-30">
+                        <div class="cat-icon">
+                            <img src="../assets/img/gallery/team2.png" alt="">
+                        </div>
+                        <div class="cat-cap">
+                            <h5><a href="#">Jullia Lorraine Nunes Silva</a></h5>
+                            <p>Estudante do curso técnico em informática integrado ao ensino médo do Instituto Federal de São Paulo - Campus Campinas</p>
+                        </div>
+                    </div>
+                </div>
+				
+				
+                <div class="col-lg-4 col-md-6 col-sm-8">
+                    <div class="single-cat text-center mb-30">
+                        <div class="cat-icon">
+                            <img src="../assets/img/gallery/team3.png" alt="">
+                        </div>
+                        <div class="cat-cap">
+                            <h5><a href="#">Isabelle Galvão Pereira</a></h5>
+                            <p>Estudante do curso técnico em informática integrado ao ensino médo do Instituto Federal de São Paulo - Campus Campinas</p>
+                        </div>
+                    </div>
+                </div>
+				
+				
+            </div>
+        </div>
+    </section>
+    <!--? Team End-->
+    <!-- Services End-->
+    <!--? About-2 Area Start -->
+    <div class="about-area2 section-padding40">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-7 col-md-12">
+                    <!-- about-img -->
+                    <div class="about-img ">
+                        <img src="../assets/img/gallery/about.png" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-5 col-md-12">
+                    <div class="about-caption mb-50">
+                        <!-- Section Tittle -->
+                        <div class="section-tittle mb-35">
+                            <h2>Inclusão e Acessibilidade</h2>
+                        </div>
+                        <p class="pera-top mb-40">***Software de compartilhamento de depoimentos para Pessoas com Deficiência (PcD)***</p>
+                        <p class="pera-bottom mb-30">descrição do projeto</p>
+						<!--
+                        <div class="icon-about">
+							<img src="../assets/img/icon/about1.svg" alt="" class=" mr-20">
+							<img src="../assets/img/icon/about2.svg" alt="">
+						</div>
+						-->
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+ <!--? Testimonial Area Start 
+ <section class="testimonial-area testimonial-padding fix">
+    <div class="container">
+        <div class="row align-items-center justify-content-center">
+            <div class=" col-lg-9">
+                <div class="about-caption">
+                    <!-- Testimonial Start 
+                    <div class="h1-testimonial-active dot-style">
+                        <!-- Single Testimonial 
+                        <div class="single-testimonial position-relative">
+                            <div class="testimonial-caption">
+                                <img src="../assets/img/icon/quotes-sign.png" alt="" class="quotes-sign">
+                                <p>"The automated process starts as soon as your clothe go into the machine. This site outcome is gleaming clothe. Placeholder text commonly used. In publishing and graphic.</p>
+                            </div>
+                            <!-- founder 
+                            <div class="testimonial-founder d-flex align-items-center">
+                                <div class="founder-img">
+                                    <img src="../assets/img/icon/testimonial.png" alt="">
+                                </div>
+                                <div class="founder-text">
+                                    <span>Robart Brown</span>
+                                    <p>Creative designer at Colorlib</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Single Testimonial 
+                        <div class="single-testimonial position-relative">
+                            <div class="testimonial-caption">
+                                <img src="../assets/img/icon/quotes-sign.png" alt="" class="quotes-sign">
+                                <p>"The automated process starts as soon as your clothe go into the machine. This site outcome is gleaming clothe. Placeholder text commonly used. In publishing and graphic.</p>
+                            </div>
+                            <!-- founder 
+                            <div class="testimonial-founder d-flex align-items-center">
+                                <div class="../founder-img">
+                                    <img src="../assets/img/icon/testimonial.png" alt="">
+                                </div>
+                                <div class="founder-text">
+                                    <span>Robart Brown</span>
+                                    <p>Creative designer at Colorlib</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Testimonial End 
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--? Testimonial Area End -->
+
+<!--? NOSSO VÍDEO EXPLICANDO O PROJETO -->
+<div class="container">
+    <div class="video-area section-bg2 d-flex align-items-center"  data-background="../assets/img/gallery/video-bg.png">
+        <div class="video-wrap position-relative">
+            <div class="video-icon" >
+                <a class="popup-video btn-icon" href="https://www.youtube.com/watch?v=up68UAfH0d0"><i class="fas fa-play"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- video_end -->  
+
+    
+<!--? Blog Area Start -->
+<section class="home-blog-area section-padding30">
+    <div class="container">
+        <!-- Section Tittle -->
+        <div class="row justify-content-center">
+            <div class="col-lg-7 col-md-9 col-sm-10">
+                <div class="section-tittle text-center mb-100">
+                    <h2>O projeto</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="home-blog-single mb-40">
+                    <div class="blog-img-cap">
+                        <div class="blog-img">
+                            <img src="../assets/img/gallery/blog1.png" alt="">
+                        </div>
+                        <div class="blog-cap">
+                            <h3><a href="blog_details.html">título</a></h3>
+                            <P>texto</P>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="home-blog-single mb-40">
+                    <div class="blog-img-cap">
+                        <div class="blog-img">
+                            <img src="../assets/img/gallery/blog2.png" alt="">
+                        </div>
+                        <div class="blog-cap">
+                            <h3><a href="blog_details.html">titulo</a></h3>
+                            <P>texto</P>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="home-blog-single mb-40">
+                    <div class="blog-img-cap">
+                        <div class="blog-img">
+                            <img src="../assets/img/gallery/blog3.png" alt="">
+                        </div>
+                        <div class="blog-cap">
+                            <h3><a href="blog_details.html">titulo</a></h3>
+                            <P>texto</P>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Blog Area End -->
+<!--? About Law Start-->
+<section class="about-low-area mt-30">
+    <div class="container">
+        <div class="about-cap-wrapper">
+            <div class="row">
+                <div class="col-xl-5  col-lg-6 col-md-10 offset-xl-1">
+                    <div class="about-caption mb-50">
+                        <!-- Section Tittle -->
+                        <div class="section-tittle mb-35">
+                            <h2>Comente algo que ache relevante</h2>
+                        </div>
+                        <p>Crítica sobre a falta de acessibilidade? Algum depoimento? Feedback sobre o projeto? Esse é um local aberto, sinta-se à vontade para falar o que quiser ;-)</p>
+                        <a href="../php/comentarios.php" class="border-btn">Faça um comentario</a>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12">
+                    <!-- about-img -->
+                    <div class="about-img">
+                        <div class="about-font-img">
+                            <img src="../assets/img/gallery/about2.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- About Law End-->
+</main>
+
+<?php
+	include ("rodape.php");
+?>
+
+<?php
+	include ("base.php");
+?>
