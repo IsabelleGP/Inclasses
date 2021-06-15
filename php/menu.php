57f4159bbf8f4a47a787667b07f946f042bf6781
IsabelleGP <69484978+IsabelleGP@users.noreply.github.com>
2021-06-15 11:41:01 -0300

    Create menu.php

---
+++ php/menu.php
@@ -0,0 +1,67 @@
+<!--? Preloader Start 
+<div id="preloader-active">
+    <div class="preloader d-flex align-items-center justify-content-center">
+        <div class="preloader-inner position-relative">
+            <div class="preloader-circle"></div>
+            <div class="preloader-img pere-text">
+                <img src="" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Preloader Start -->
+
+<!-- CABEÇALHO -->
+    <header>
+        <!--? Header Start -->
+        <div class="header-area">
+            <div class="main-header header-sticky">
+                <div class="container-fluid">
+                    <div class="row align-items-center">
+                       
+					   <!-- Logo -->
+                        <div class="col-xl-2 col-lg-2 col-md-1">
+                            <div class="logo">
+                                <a href="../index.php"><img src="../assets/img/logo/logo.png" alt=""></a>
+                            </div>
+                        </div>
+                        
+						<!-- Menu -->
+						<div class="col-xl-10 col-lg-10 col-md-10">
+                            <div class="menu-main d-flex align-items-center justify-content-end">                              
+                                <div class="main-menu f-right d-none d-lg-block">
+                                    <nav> 
+                                        <ul id="navigation">
+                                            <li><a href="../php/index.php">Página inicial</a></li>
+                                            <li><a href="../php/sobre.php">Sobre</a></li>
+											<!--
+                                            <li><a href="services.html">Services</a></li>
+                                            <li><a href="blog.html">Blog</a>
+                                                <ul class="submenu">
+                                                    <li><a href="blog.html">Blog</a></li>
+                                                    <li><a href="blog_details.html">Blog Details</a></li>
+                                                    <li><a href="elements.html">Element</a></li>
+                                                </ul>
+                                            </li>
+											-->
+                                            <li><a href="../php/cadastro.php">Realizar Cadastro</a></li>
+											<li><a href="../php/excluir.php">Excluir Cadastro</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>																					
+                                <div class="header-right-btn f-right d-none d-lg-block ml-15">
+                                    <a href="../php/comentarios.php" class="btn header-btn">Realizar um comentário</a>
+                                </div>
+                            </div>
+                        </div> 
+						
+                        <!-- Mobile Menu -->
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+	<!-- FIM DO CABEÇALHO -->
