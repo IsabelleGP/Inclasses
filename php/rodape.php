98f77537c197baee2b7b486d3c94b9200279c975
IsabelleGP <69484978+IsabelleGP@users.noreply.github.com>
2021-06-15 11:41:13 -0300

    Create rodape.php

---
+++ php/rodape.php
@@ -0,0 +1,83 @@
+<!-- PARTE 8 -->
+<footer>
+    <div class="footer-wrappr section-bg3" data-background="assets/img/gallery/footer-bg.png">
+        <div class="footer-area footer-padding ">
+            <div class="container">
+                <div class="row justify-content-between">
+                    <div class="col-xl-8 col-lg-8 col-md-6 col-sm-12">
+                        <div class="single-footer-caption mb-50">
+						
+                            <!-- LOGO -->
+                            <div class="footer-logo mb-25">
+                                <a href="../index.php"><img src="assets/img/logo/logo2_footer.png" alt=""></a>
+                            </div>
+							
+                            <div class="header-area">
+                                <div class="main-header main-header2">
+                                    <div class="menu-main d-flex align-items-center justify-content-start">
+                                        <!-- MENU -->
+                                        <div class="main-menu main-menu2">
+                                            <nav> 
+                                                <ul>
+                                                    <li><a href="../php/index.php">Página inicial</a></li>
+                                                    <li><a href="../php/sobre.php">Sobre</a></li>
+                                                    <li><a href="../php/cadastro.php">Cadastro</a></li>
+													<li><a href="../php/comentarios.php">Comentários</a></li>
+                                                </ul>
+                                            </nav>
+                                        </div>
+                                    </div>  
+                                </div>
+                            </div>
+							
+                            <!-- ÍCONES DE REDES SOCIAIS -->
+                            <div class="footer-social mt-50">
+                                <a href="#"><i class="fab fa-twitter"></i></a>
+                                <a href="https://bit.ly/sai4ull"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                        <div class="single-footer-caption">
+                            <div class="footer-tittle mb-50">
+                                <h4>Inclusão e Acessibilidade</h4>
+                            </div>
+							
+                            <div class="footer-tittle">
+                                <div class="footer-pera">
+                                    <p>Susan Francalassi</p>
+									<p>Jullia Nunes</p>
+									<p>Isabelle Galvão</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+		
+			<!-- CRÉDITOS -->
+			<div class="footer-bottom-area">
+				<div class="container">
+					<div class="footer-border">
+						<div class="row">
+							<div class="col-xl-10 ">
+								<div class="footer-copy-right">
+									<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+									Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+									<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+							    </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+  </div>
+ <!-- FIM DA PARTE 8 -->
+ 
+  <!-- Scroll Up -->
+  <div id="back-top" >
+    <a title="Go to Top" href="#"> <i class="fas fa-level-up-alt"></i></a>
+</div>
